f03f3527cebd0d2d05da90f044b10adc3663fd8a
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2025-10-24 09:19:08 -0400

    use envvars to get username and password

--- R/authenticate.R
+++ R/authenticate.R
@@ -3,40 +3,42 @@ if (!exists("credentials", inherits = FALSE)) {
 }
 
 #' Get login information for the BWG database
-#' 
-#' This function retrieves a login token necessary for accessing the BWG database. 
-#' It prompts the user for their BWG username and password (the password is masked), 
-#' and then sends a login request to the API to obtain an authentication token. 
+#'
+#' This function retrieves a login token necessary for accessing the BWG database.
+#' It prompts the user for their BWG username and password (the password is masked),
+#' and then sends a login request to the API to obtain an authentication token.
 #' This token is saved in the global environment variable `credentials` for later use.
-#' 
+#'
 #' @details
-#' The login information is stored in the `credentials` environment, specifically 
-#' in the `token` field. The token is required for making subsequent requests 
+#' The login information is stored in the `credentials` environment, specifically
+#' in the `token` field. The token is required for making subsequent requests
 #' to the BWG database API.
-#' 
-#' @return This function does not return a value but sets the `credentials$token` 
+#'
+#' @return This function does not return a value but sets the `credentials$token`
 #'   environment variable with the obtained login token.
-#' 
+#'
 #' @export
 bwg_auth <- function() {
-  ## should check if it exists, and not rerun the function.
-  
-  username <- readline("USERNAME: ")
-  password <- getPass::getPass("PASSWORD: ")
-  
-  ## create POST 
+  # Try to read environment variables
+  env_user <- Sys.getenv("bwg_username", unset = NA)
+  env_pass <- Sys.getenv("bwg_password", unset = NA)
+
+  if (!is.na(env_user) && !is.na(env_pass)) {
+    username <- env_user
+    password <- env_pass
+  } else {
+    message("Environment variables 'bwg_username' and/or 'bwg_password' not set. Please enter credentials manually.")
+    username <- readline("USERNAME: ")
+    password <- getPass::getPass("PASSWORD: ")
+  }
+
+  # Create POST request
   url <- "https://www.zoology.ubc.ca/~srivast/bwgdb/v3/api/?route=users&action=login"
-  
-  body <- list(username = username,
-               password = password)
-  
+  body <- list(username = username, password = password)
+
   r <- httr::POST(url, body = body, encode = "json")
-  
-  ## check if the request was successful
   httr::stop_for_status(r)
-  
+
   answer <- httr::content(r, as = "text")
-  
-  ## store the token in the credentials environment
-  credentials$token  <- jsonlite::fromJSON(answer)[["results"]]
+  credentials$token <- jsonlite::fromJSON(answer)[["results"]]
 }
