7c9518fb34742f7a325ea0e9cfccd9dbebfa8a5a
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2025-11-12 12:00:59 -0500

    exposes get_all_abundances function

--- R/species.R
+++ R/species.R
@@ -1,51 +1,51 @@
 #' Get data from bwg
-#' 
-#' At present this is very general, since perhaps we will 
+#'
+#' At present this is very general, since perhaps we will
 #' only ever get whole tables?
-#' 
+#'
 #' @param dataname the name of the dataset you want (e.g.
 #'   "species")
 #' @param opts list of named options for the different API routes.
-#' @param to_dataframe logical, should the response be converted to a 
-#'   data frame? Default is TRUE. If FALSE, the raw content of the response 
+#' @param to_dataframe logical, should the response be converted to a
+#'   data frame? Default is TRUE. If FALSE, the raw content of the response
 #'   is returned as text.
-#'  
+#'
 #' @return a data.frame containing one whole table.
 #' @export
 bwg_get <- function(dataname, opts = NULL, to_dataframe = TRUE) {
-  
+
   ## first, check if there is a token
   if (!exists("token", envir = credentials)) {
     message("please log in first!")
     bwg_auth()
   }
-  
+
   token <- get("token", envir = credentials)
-  
+
   ## process token
   token_value <- token[[1]]
   bearer <- paste0("bearer ", token_value)
-  
+
   ## request the species
   baseurl <- "https://www.zoology.ubc.ca/~srivast/bwgdb/v3/api/"
   list_method <- list(route = dataname, action = "list")
   q <- c(list_method, opts)
-  
+
   response <- httr::GET(baseurl,
                         httr::add_headers(Authorization = bearer),
                         query = q)
-  
+
   ## error if it didn't work
   httr::stop_for_status(response)
-  
+
   ## is it json?
   is_json <- grepl("json", httr::headers(response)$`content-type`)
   content <- httr::content(response, as = "text")
-  
+
   if (isTRUE(to_dataframe)) {
     ## Parse response
     response_data <- jsonlite::fromJSON(content, flatten = TRUE)
-    
+
     ## Determine how to extract data
     if (is.data.frame(response_data$results)) {
       # If results is already a data frame, convert to tibble
@@ -60,6 +60,31 @@ bwg_get <- function(dataname, opts = NULL, to_dataframe = TRUE) {
   } else {
     output <- content
   }
-  
+
   output
+}
+
+
+
+#' Get all Abundances from a list of datasets
+#'
+#' @param .dats a dataframe containing datasets. usually the result of using
+#'   `bwg_get("datasets")`
+#'
+#' @returns a dataframe with one row per invertebrate species found in each
+#'   dataset. abundances across all bromeliads are in a NESTED dataframe to
+#'   reduce clutter.
+#' @export
+get_all_abundances <- function(.dats){
+
+  ## might need to replace this with a suitable structure
+  bwg_get_safe <- purrr::possibly(bwg_get, NA)
+
+  abds_ <- .dats[["dataset_id"]] |>
+    as.numeric() |>
+    purrr::set_names() |>
+    purrr::map(~ list(dataset_id = .x)) |>
+    purrr::map(~ bwg_get_safe("matrix", .))
+
+  return(abds_)
 }
--- R/tidy-abundance.R
+++ R/tidy-abundance.R
@@ -1,83 +1,98 @@
 #' Tidy a list of abundance data into a data.frame
-#' 
-#' This function processes data from the output of \code{bwg_get("matrix")}. It 
-#' runs several smaller functions that convert a list of nested datasets into 
-#' a tidy data frame. The function assumes that the input is a list of 
-#' datasets, and it heavily utilizes the \code{purrr} package for mapping and 
+#'
+#' This function processes data from the output of \code{bwg_get("matrix")}. It
+#' runs several smaller functions that convert a list of nested datasets into
+#' a tidy data frame. The function assumes that the input is a list of
+#' datasets, and it heavily utilizes the \code{purrr} package for mapping and
 #' iterating over the data.
-#' 
-#' @param matrix_data_list A list of datasets containing abundance data. 
-#'   Each dataset is assumed to have measurements related to various species 
+#'
+#' @param matrix_data_list A list of datasets containing abundance data.
+#'   Each dataset is assumed to have measurements related to various species
 #'   and their respective bromeliad abundances.
-#' 
-#' @return A tidy data.frame where each row represents an individual 
-#'   observation of abundance data, with columns representing species, 
+#'
+#' @return A tidy data.frame where each row represents an individual
+#'   observation of abundance data, with columns representing species,
 #'   measurements, and dataset metadata.
 #' @export
 tidy_dataset_list <- function(matrix_data_list){
-  matrix_data_list %>%
-    map(make_brom_a_df) %>%
-    map(make_full_df) %>%
-    bind_rows(.id = "dataset_id")
+  matrix_data_list |>
+    purrr::map_df(make_brom_a_df, .id = "dataset_id")
 }
 
 
 #' Process abundance data for bromeliads and convert to a data frame
-#' 
-#' This function processes abundance data at a depth of 2 in a nested list 
-#' and returns a data frame for each dataset. It extracts measurements related 
+#'
+#' This function processes abundance data at a depth of 2 in a nested list
+#' and returns a data frame for each dataset. It extracts measurements related
 #' to bromeliads and calculates abundance values.
-#' 
+#' Leaving the measurements column nested for a time so that
+#'
 #' @param abd_data A nested list containing abundance data.
-#' 
-#' @return A nested data frame where each element corresponds to the 
+#'
+#' @return A nested data frame where each element corresponds to the
 #'   abundance of a species in a particular bromeliad.
 #' @export
 make_brom_a_df <- function(abd_data){
-  abd_data %>%
-    at_depth(2,
-             ~ map_at(.x, "measurements",
-                      ~ map(.x,
-                            ~ map_at(.x, "bromeliads",
-                                     ~ data_frame(bromeliad_id = names(.x),
-                                                  abd = test_numeric(.x))))))
+
+
+  abd_data_flat <- purrr:::flatten_df(abd_data)
+
+  aa <- abd_data_flat |>
+    dplyr::mutate(measurements =
+             purrr:::map(measurements,
+                 flatten_one_sp_x_bromeliads)
+    ) |>
+    dplyr::mutate(species_id = names(measurements))
+
+  return(aa)
+
+}
+
+flatten_one_sp_x_bromeliads <- function(.x){
+  .x |>
+    tibble::as_tibble() |>
+    dplyr::mutate(measurement = purrr::flatten_chr(measurement),
+           bromeliads = tibble::enframe(bromeliads,
+                                name = "bromeliad_id",
+                                value = "abd")) |>
+    tidyr::unnest(bromeliads)
 }
 
 
 #' Convert a list of measurements into a single data frame
-#' 
-#' This function processes a list of measurements for a specific insect size 
+#'
+#' This function processes a list of measurements for a specific insect size
 #' and returns them as a flat data frame.
-#' 
-#' @param measure_list A list containing measurements for a particular insect 
+#'
+#' @param measure_list A list containing measurements for a particular insect
 #'   size.
-#' 
+#'
 #' @return A data frame representing the measurements for a specific insect size.
 #' @export
 one_measurement <- function(measure_list){
   measure_list_flat <- map_at(measure_list, "measurement", flatten_chr)
-  
+
   assertthat::assert_that(length(measure_list_flat$measurement) == 1)
-  
-  measure_list_flat %>%
-    flatten %>%
-    invoke(data_frame, .)
+
+  measure_list_flat |>
+    purrr::flatten() |>
+    purrr:::invoke(data_frame, .)
 }
 
 
 #' Test if numeric values are valid
-#' 
-#' This function converts input values into numeric format and stores any 
+#'
+#' This function converts input values into numeric format and stores any
 #' values that are not valid numbers (NA) into a list of problems.
-#' 
+#'
 #' @param to_num A vector of values to be converted to numeric.
-#' 
-#' @return A numeric vector where invalid values are converted to NA. 
+#'
+#' @return A numeric vector where invalid values are converted to NA.
 #'   It also updates the 'weird' environment with the invalid entries.
 #' @export
 test_numeric <- function(to_num){
   as_num <- as.numeric(to_num)
-  
+
   for (x in 1:length(as_num))
     if (is.na(as_num[x])) {
       weird$problems <- c(weird$problems, to_num[x])
